f4a4c32afcbf9c4d9a9a0824f4697ac3cd3d4cc9
sakshij1512 <jaiswalsakshi1512@gmail.com>
2025-09-04 17:39:50 +0530

    "removed unecessary files"

--- server/src/db.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-
-export async function connectDB(uri) {
-if (!uri) throw new Error('MONGO_URI missing');
-mongoose.set('strictQuery', true);
-await mongoose.connect(uri);
-return mongoose.connection;
-}
